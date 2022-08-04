84c2b9a547f268a533bf96e10e61049dc23e57b9
ArenRitz <aren.dolbakian@gmail.com>
2022-08-04 17:00:35 -0400

    fix submit page

--- routes/submit.js
+++ routes/submit.js
@@ -14,6 +14,7 @@ module.exports = (db, client) => {
     getPoll(db, req.params.id)
       .then((data) => {
         const templateVar = { poll: data };
+        console.log(templateVar);
         res.render("submit", templateVar);
         return;
       })
